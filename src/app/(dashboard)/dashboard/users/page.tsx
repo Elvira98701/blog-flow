df0a6d2455cf2b2fdbe0194f91b64c4ff5c0db45
Elvira Latypova <ellalatypova666@gmail.com>
2025-10-09 16:09:12 +0500

    perf: reduce unnecessary re-renders in users components

--- src/app/(dashboard)/dashboard/users/page.tsx
+++ src/app/(dashboard)/dashboard/users/page.tsx
@@ -1,10 +1,19 @@
 import { Gradient } from "@/components/shared";
 import { FeedUsers } from "@/features/users";
+import { UsersSearchInput } from "@/features/users/users-search-input";
 
 export default function Users() {
   return (
     <div className="flex justify-center relative">
-      <FeedUsers />
+      <section className="w-full">
+        <div className="flex justify-between items-center">
+          <h2>Top Users</h2>
+          <UsersSearchInput />
+        </div>
+
+        <FeedUsers />
+      </section>
+
       <Gradient className="fixed top-0 right-0 -z-10" />
     </div>
   );
--- src/features/users/feed-users/feed-users.tsx
+++ src/features/users/feed-users/feed-users.tsx
@@ -4,61 +4,53 @@ import { useSession } from "next-auth/react";
 
 import { UserCard } from "@/components/shared";
 import { Loader, Skeleton, ErrorText } from "@/components/ui";
-import { cn } from "@/lib/utils";
-
-import { UsersSearchInput } from "../users-search-input";
 
 import { useInfiniteUsers } from "./use-infinite-users";
 
-interface FeedUsersProps {
-  className?: string;
-}
-
-export const FeedUsers = ({ className }: FeedUsersProps) => {
+export const FeedUsers = () => {
   const { data: session } = useSession();
   const { data, error, isLoading, isError, lastRowRef, isFetchingNextPage } =
     useInfiniteUsers();
 
-  return (
-    <section className={cn("w-full", className)}>
-      <div className="flex justify-between items-center">
-        <h2>Top Users</h2>
-        <UsersSearchInput />
+  if (isLoading) {
+    return (
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-5">
+        {Array.from({ length: 8 }, (_, i) => (
+          <Skeleton key={i} className="w-full rounded-lg h-80 border" />
+        ))}
       </div>
+    );
+  }
 
-      {isLoading ? (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-5">
-          {Array.from({ length: 8 }, (_, i) => (
-            <Skeleton key={i} className="w-full rounded-lg h-80 border" />
-          ))}
+  if (isError) {
+    return (
+      <ErrorText text={error?.message ?? ""} size="lg" className="mt-10" />
+    );
+  }
+
+  return (
+    <>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-5">
+        {data?.pages.map((page, pageIndex) => {
+          return page.users.map((user, userIndex) => {
+            const isLastPage = pageIndex === data.pages.length - 1;
+            const isLastPost = userIndex === page.users.length - 1;
+            const ref = isLastPage && isLastPost ? lastRowRef : null;
+
+            return (
+              <div key={user.id} ref={ref}>
+                <UserCard user={user} session={session} />
+              </div>
+            );
+          });
+        })}
+      </div>
+      {isFetchingNextPage && <Loader className="py-4" />}
+      {data?.pages[0].users.length === 0 && (
+        <div className="flex items-center min-h-[80vh]">
+          <p>There are no users yet</p>
         </div>
-      ) : isError ? (
-        <ErrorText text={error?.message ?? ""} size="lg" className="mt-10" />
-      ) : (
-        <>
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-5">
-            {data?.pages.map((page, pageIndex) => {
-              return page.users.map((user, userIndex) => {
-                const isLastPage = pageIndex === data.pages.length - 1;
-                const isLastPost = userIndex === page.users.length - 1;
-                const ref = isLastPage && isLastPost ? lastRowRef : null;
-
-                return (
-                  <div key={user.id} ref={ref}>
-                    <UserCard user={user} session={session} />
-                  </div>
-                );
-              });
-            })}
-          </div>
-          {isFetchingNextPage && <Loader className="py-4" />}
-          {data?.pages[0].users.length === 0 && (
-            <div className="flex items-center min-h-[80vh]">
-              <p>There are no users yet</p>
-            </div>
-          )}
-        </>
       )}
-    </section>
+    </>
   );
 };
