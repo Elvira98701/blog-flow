9a9e4e569101ab6cb5f5d446fa0675ffcd7f7f2b
Elvira Latypova <ellalatypova666@gmail.com>
2025-10-10 08:57:20 +0500

    refactor: stabilize infinite scroll with sentinel element and memoized posts

--- src/features/posts/feed-posts/feed-posts.tsx
+++ src/features/posts/feed-posts/feed-posts.tsx
@@ -6,7 +6,7 @@ import { Loader, Skeleton, ErrorText } from "@/components/ui";
 import { useInfinitePosts } from "./use-infinite-posts";
 
 export const FeedPosts = () => {
-  const { data, error, isLoading, isError, lastRowRef, isFetchingNextPage } =
+  const { posts, error, isLoading, isError, lastRowRef, isFetchingNextPage } =
     useInfinitePosts();
 
   if (isLoading) {
@@ -28,22 +28,15 @@ export const FeedPosts = () => {
   return (
     <>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 mt-5">
-        {data?.pages.map((page, pageIndex) => {
-          return page.posts.map((post, postIndex) => {
-            const isLastPage = pageIndex === data.pages.length - 1;
-            const isLastPost = postIndex === page.posts.length - 1;
-            const ref = isLastPage && isLastPost ? lastRowRef : null;
-
-            return (
-              <div key={post.id} ref={ref}>
-                <PostCard post={post} />
-              </div>
-            );
-          });
+        {posts.map((post) => {
+          return <PostCard key={post.id} post={post} />;
         })}
       </div>
-      {isFetchingNextPage && <Loader className="py-4" />}
-      {data?.pages[0].posts.length === 0 && (
+      <div ref={lastRowRef}>
+        {isFetchingNextPage && <Loader className="py-4" />}
+      </div>
+
+      {posts.length === 0 && (
         <div className="flex items-center min-h-[80vh]">
           <p>There are no posts yet</p>
         </div>
