085696d76c41cc7f19bbf7d12f4802771008f97f
Elvira Latypova <ellalatypova666@gmail.com>
2025-10-10 09:07:43 +0500

    refactor: move useMemo to use-infinite-posts.ts

--- src/features/posts/posts-by-user/posts-by-user.tsx
+++ src/features/posts/posts-by-user/posts-by-user.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useMemo } from "react";
-
 import { BigPostCard } from "@/components/shared";
 import { Loader, Skeleton, ErrorText } from "@/components/ui";
 import { cn } from "@/lib/utils";
@@ -19,12 +17,8 @@ export const PostsByUser = ({
   sessionUserId,
   className,
 }: PostsByUserProps) => {
-  const { data, error, isLoading, isError, lastRowRef, isFetchingNextPage } =
+  const { posts, error, isLoading, isError, lastRowRef, isFetchingNextPage } =
     useInfinitePostsByUser(userId);
-
-  const posts = useMemo(() => {
-    return data?.pages.flatMap((page) => page.posts) ?? [];
-  }, [data?.pages]);
 
   return (
     <div className={cn("flex flex-col gap-4 items-center", className)}>
@@ -48,7 +42,7 @@ export const PostsByUser = ({
       )}
       <div ref={lastRowRef}>{isFetchingNextPage && <Loader />}</div>
 
-      {data?.pages[0].posts.length === 0 && (
+      {posts.length === 0 && (
         <div className="flex items-center justify-center min-h-[50vh] border w-full bg-card rounded-md ">
           <p>The user doesn&apos;t have any posts yet</p>
         </div>
